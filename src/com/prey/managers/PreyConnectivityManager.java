1da9371b24a0349b161cefb19a867b2acf961901
oraliaga <oraliaga@gmail.com>
2013-04-16 18:06:38 -0300

    Null control

--- src/com/prey/managers/PreyConnectivityManager.java
+++ src/com/prey/managers/PreyConnectivityManager.java
@@ -5,39 +5,47 @@ import android.net.ConnectivityManager;
 
 public class PreyConnectivityManager {
 
- 
-	private ConnectivityManager connectivity=null;
+	private ConnectivityManager connectivity = null;
 	private static PreyConnectivityManager _instance = null;
-	
+
 	private PreyConnectivityManager(Context ctx) {
 		connectivity = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
 	}
-	
+
 	public static PreyConnectivityManager getInstance(Context ctx) {
 		if (_instance == null)
 			_instance = new PreyConnectivityManager(ctx);
 		return _instance;
 	}
-	
-	
-	public boolean isConnected(){
-		return connectivity.getActiveNetworkInfo().isConnected();
+
+	public boolean isConnected() {
+
+		if (connectivity.getActiveNetworkInfo() != null)
+			return connectivity.getActiveNetworkInfo().isConnected();
+		return false;
 	}
 
-	public boolean isAvailable(){
-		return connectivity.getActiveNetworkInfo().isAvailable();
+	public boolean isAvailable() {
+		if (connectivity.getActiveNetworkInfo() != null)
+			return connectivity.getActiveNetworkInfo().isAvailable();
+		return false;
 	}
-	
-	public boolean isConnectedOrConnecting(){
-		return connectivity.getActiveNetworkInfo().isConnectedOrConnecting();
+
+	public boolean isConnectedOrConnecting() {
+		if (connectivity.getActiveNetworkInfo() != null)
+			return connectivity.getActiveNetworkInfo().isConnectedOrConnecting();
+		return false;
 	}
-	
-	public boolean isFailover(){
-		return connectivity.getActiveNetworkInfo().isFailover();
+
+	public boolean isFailover() {
+		if (connectivity.getActiveNetworkInfo() != null)
+			return connectivity.getActiveNetworkInfo().isFailover();
+		return false;
 	}
-	
-	public boolean isRoaming(){
-		return connectivity.getActiveNetworkInfo().isRoaming();
+
+	public boolean isRoaming() {
+		if (connectivity.getActiveNetworkInfo() != null)
+			return connectivity.getActiveNetworkInfo().isRoaming();
+		return false;
 	}
 }
- 
