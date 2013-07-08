eafea907a2a119bf3ed52ddefdd60cb98c5e3765
oraliaga <oraliaga@gmail.com>
2013-07-08 01:12:26 -0400

    New Receiver Battery

---
+++ src/com/prey/receivers/BatteryReceiver.java
@@ -0,0 +1,29 @@
+package com.prey.receivers;
+
+import com.prey.PreyLogger;
+
+import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.BatteryManager;
+import android.os.Build;
+
+public class BatteryReceiver extends BroadcastReceiver{
+
+	@TargetApi(Build.VERSION_CODES.ECLAIR) @Override
+	public void onReceive(Context context, Intent intent) {
+		int scale = -1;
+		int level = -1;
+		int voltage = -1;
+		int temp = -1;
+
+		level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+		scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+		temp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1);
+		voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
+		PreyLogger.d("BatteryLevelReceiver level is " + level + "/" + scale + ", temp is " + temp + ", voltage is " + voltage);
+		
+	}
+
+}
