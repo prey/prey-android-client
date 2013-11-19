4f6f7008f1c07464f901a110bec54cb641c69182
Orlando Aliaga <oaliaga@gmail.com>
2013-11-19 17:10:46 -0300

    not shutdown, is locked

---
+++ src/com/prey/json/actions/Keepon.java
@@ -0,0 +1,33 @@
+package com.prey.json.actions;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.prey.PreyConfig;
+import com.prey.PreyLogger;
+import com.prey.actions.observer.ActionResult;
+import com.prey.services.PreyKeepOnService;
+
+public class Keepon {
+
+	public void sms(Context ctx, List<ActionResult> lista, JSONObject parameters) {
+		try {
+            String parameter = parameters.getString("parameter");
+            boolean keepOn= parameter!=null&&("on".equals(parameter)||"true".equals(parameter));
+            PreyLogger.i("keepOn:"+keepOn);
+            PreyConfig.getPreyConfig(ctx).setKeepOn(keepOn);
+            Intent intent = new Intent(ctx, PreyKeepOnService.class);
+            if(keepOn){
+            	 ctx.startService(intent);
+            }else{
+	             ctx.stopService(intent);
+            }
+		} catch (Exception e) {
+            PreyLogger.i("Error causa:" + e.getMessage() );
+		} 
+	}
+}
---
+++ src/com/prey/receivers/PreyKeepOnReceiver.java
@@ -0,0 +1,48 @@
+package com.prey.receivers;
+
+import com.prey.PreyConfig;
+import com.prey.services.PreyKeepOnService;
+
+import android.annotation.TargetApi;
+import android.app.KeyguardManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.PowerManager;
+
+public class PreyKeepOnReceiver extends BroadcastReceiver {
+
+	public PreyKeepOnReceiver() {
+	}
+
+	@TargetApi(Build.VERSION_CODES.ECLAIR_MR1)
+	public void onReceive(Context context, Intent intent) {
+		boolean keepOn = PreyConfig.getPreyConfig(context).isKeepOn();
+		if (keepOn) {
+			if ("android.intent.action.CLOSE_SYSTEM_DIALOGS".equals(intent.getAction())) {
+				boolean flag = ((KeyguardManager) context.getSystemService("keyguard")).inKeyguardRestrictedInputMode();
+				if (flag) {
+					PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+					boolean isScreenOn = pm.isScreenOn();
+					if (isScreenOn) {
+						try {
+							Thread.sleep(400);
+						} catch (InterruptedException e) {
+						}
+						Intent intentClose = new Intent("android.intent.action.CLOSE_SYSTEM_DIALOGS");
+						context.sendBroadcast(intentClose);
+					}
+				}
+			}
+			if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+				try {
+					Intent intentKeepOn = new Intent(context, PreyKeepOnService.class);
+					context.startService(intentKeepOn);
+				} catch (Exception e) {
+				}
+			}
+		}
+	}
+
+}
