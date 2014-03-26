d41bd8ab458005322658e3686b2d5324d2825a31
Orlando Aliaga <oaliaga@gmail.com>
2014-03-26 16:26:09 -0300

    Comment  to receiver

--- src/com/prey/receivers/BatteryLevelReceiver.java
+++ src/com/prey/receivers/BatteryLevelReceiver.java
@@ -1,22 +1,14 @@
 package com.prey.receivers;
 
-import java.util.HashMap;
-
-import com.prey.PreyLogger;
-import com.prey.actions.location.PreyLocation;
-import com.prey.actions.location.PreyLocationManager;
-import com.prey.services.LocationService;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
-import android.os.BatteryManager;
 
 public class BatteryLevelReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context ctx, Intent intent) {
+		/*
 		int scale = -1;
 		int level = -1;
 		int voltage = -1;
@@ -28,31 +20,12 @@ public class BatteryLevelReceiver extends BroadcastReceiver {
 		voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
 		PreyLogger.d("BatteryLevelReceiver level is " + level + "/" + scale + ", temp is " + temp + ", voltage is " + voltage);
 
-		ctx.startService(new Intent(ctx, LocationService.class));
 		
-		boolean validLocation = false;
-		PreyLocation lastLocation=null;
-		HashMap<String, String> parameters = new HashMap<String, String>(); 
-		while (!validLocation) {
-			lastLocation = PreyLocationManager.getInstance(ctx).getLastLocation();
-			if (lastLocation.isValid()) {
-				validLocation = true;
-				parameters.put("lat", Double.toString(lastLocation.getLat()));
-				parameters.put("lng", Double.toString(lastLocation.getLng()));
-				parameters.put("acc", Float.toString(lastLocation.getAccuracy()));
-				parameters.put("alt", Double.toString(lastLocation.getAltitude()));
-			} else
-				try {
-					Thread.sleep(5000);
-				} catch (InterruptedException e) {
-					PreyLogger.e("Thread was intrrupted. Finishing Location NotifierAction", e);
-				}
-		}
-		PreyLogger.d("lat:" + Double.toString(lastLocation.getLat()));
-		PreyLogger.d("lng:" + Double.toString(lastLocation.getLng()));
-		PreyLogger.d("acc:" + Float.toString(lastLocation.getAccuracy()));
-		PreyLogger.d("alt:" + Double.toString(lastLocation.getAltitude()));
-
+		HashMap<String, String> parameters=LocationUtil.dataLocation(ctx).getDataList();
+		PreyLogger.d("lat:" + parameters.get(LocationUtil.LAT));
+		PreyLogger.d("lng:" + parameters.get(LocationUtil.LNG));
+		PreyLogger.d("acc:" + parameters.get(LocationUtil.ACC));
+		*/
 	}
 
 }
