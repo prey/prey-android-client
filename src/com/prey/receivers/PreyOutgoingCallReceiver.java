20957ecf05bce95cfc0dd67b1992e6dc4df401d2
Orlando Aliaga <oaliaga@gmail.com>
2014-06-25 17:19:40 -0400

    Disable receiver, new outgoing call

--- src/com/prey/receivers/PreyOutgoingCallReceiver.java
+++ src/com/prey/receivers/PreyOutgoingCallReceiver.java
@@ -3,7 +3,6 @@ package com.prey.receivers;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-import com.prey.PreyLogger;
 import com.prey.actions.camouflage.Camouflage;
 import com.prey.actions.camouflage.VolumeCounter;
 
@@ -15,30 +14,13 @@ import android.os.Bundle;
 
 public class PreyOutgoingCallReceiver extends BroadcastReceiver {
 
-	static final String ACTION = "android.intent.action.NEW_OUTGOING_CALL";
+ 
 	static final String VOLUME_CHANGED_ACTION = "android.media.VOLUME_CHANGED_ACTION";
 
-	SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmss");
+	private SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmss");
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		//PreyLogger.d("Out call Broadcast - Action received: "+intent.getAction());
-		if (intent.getAction() != null && intent.getAction().equals(ACTION)) {
-			
-			String phoneNumber = getResultData();
-		    if (phoneNumber != null) {
-		      // No reformatted number, use the original
-		      phoneNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-		      PreyLogger.d("phoneNumber:"+phoneNumber);
-		      if("#12345*".equals(phoneNumber)){
-		    	  Camouflage.hide(context, null, null);
-		      }
-		      if("#67890*".equals(phoneNumber)){
-		    	  Camouflage.unhide(context, null, null);
-		      }
-		    }
-		}
-		
 		 
 		if (intent.getAction() != null && intent.getAction().equals(VOLUME_CHANGED_ACTION)) {
 			Bundle bu = intent.getExtras();
