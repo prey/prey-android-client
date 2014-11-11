f6c86eb3b673dc20b42315083a60f901772f51bf
Orlando Aliaga <oaliaga@gmail.com>
2014-11-11 17:01:26 -0300

    New version 1.2.0

--- src/com/prey/PreyConfig.java
+++ src/com/prey/PreyConfig.java
@@ -107,7 +107,7 @@ public class PreyConfig {
 	public static final String SEND_NOTIFICATION_ID="SEND_NOTIFICATION_ID";
 	public static final String SIGNAL_FLARE_DATE="SIGNAL_FLARE_DATE";
 	
-	public static final String VERSION_PREY_DEFAULT="1.3.1";
+	public static final String VERSION_PREY_DEFAULT="1.2.0";
 	
 	public static final String SEND_DATA="SEND_DATA";
 	
