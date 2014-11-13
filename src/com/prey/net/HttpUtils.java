6fb57de3551f6bed6881bdab33c3c612f4e8c918
Orlando Aliaga <oaliaga@gmail.com>
2014-11-13 18:48:26 -0300

    Change SSLSocketFactory

--- src/com/prey/net/HttpUtils.java
+++ src/com/prey/net/HttpUtils.java
@@ -27,9 +27,9 @@ public class HttpUtils {
 		try {
 			KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
 			trustStore.load(null, null);
-
-			SSLSocketFactory sf = new EasySSLSocketFactory(trustStore);
-			sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+			
+			SSLSocketFactory sf = SSLSocketFactory.getSocketFactory();
+			sf.setHostnameVerifier(SSLSocketFactory.STRICT_HOSTNAME_VERIFIER);
 
 			HttpParams params = new BasicHttpParams();
 			HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
