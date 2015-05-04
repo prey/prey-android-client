30a03db6fdce9b82967e0ab9ef068d095ce8a074
Orlando Aliaga <oaliaga@gmail.com>
2015-05-04 20:04:56 -0300

    Fix #85

--- src/com/prey/activities/CheckPasswordActivity.java
+++ src/com/prey/activities/CheckPasswordActivity.java
@@ -6,11 +6,18 @@
  ******************************************************************************/
 package com.prey.activities;
 
+import java.util.Locale;
+
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.prey.PreyConfig;
 import com.prey.PreyLogger;
 import com.prey.R;
 import com.prey.backwardcompatibility.FroyoSupport;
@@ -23,6 +30,8 @@ public class CheckPasswordActivity extends PasswordActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.password);
+		
+		
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		updateLoginScreen();
 		bindPasswordControls();
@@ -42,6 +51,7 @@ public class CheckPasswordActivity extends PasswordActivity {
 				ImageView logoImgTextPrey = (ImageView) findViewById(R.id.logo_img_reversed);
 				ImageView logoImgExtra = (ImageView) findViewById(R.id.logo_img_extra);
 				TextView textReady = (TextView) findViewById(R.id.device_ready_h1_text);
+				TextView textForgotPassword= (TextView) findViewById(R.id.link_forgot_password);
 				if (keyboardVisible) {
 					PreyLogger.d("key on");
 					if (logoImgTextPrey != null)
@@ -50,6 +60,8 @@ public class CheckPasswordActivity extends PasswordActivity {
 						logoImgExtra.setVisibility(View.GONE);
 					if (textReady != null)
 						textReady.setVisibility(View.GONE);
+					if (textForgotPassword != null)
+						textForgotPassword.setVisibility(View.GONE);
 				} else {
 					PreyLogger.d("key off");
 					if (logoImgTextPrey != null)
@@ -58,9 +70,50 @@ public class CheckPasswordActivity extends PasswordActivity {
 						logoImgExtra.setVisibility(View.VISIBLE);
 					if (textReady != null)
 						textReady.setVisibility(View.VISIBLE);
+					if (textForgotPassword != null)
+						textForgotPassword.setVisibility(View.VISIBLE);
 				}
 			}
 		});
+		
+		
+		try {
+			TextView textForgotPassword= (TextView) findViewById(R.id.link_forgot_password);
+			textForgotPassword.setOnClickListener(new View.OnClickListener() {
+
+				public void onClick(View v) {
+					try {
+						String url = PreyConfig.getPreyConfig(getApplicationContext()).getPreyPanelUrl();
+						Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse(url));
+						startActivity(browserIntent);
+					} catch (Exception e) {
+					}
+					
+				}  
+			});
+		} catch (Exception e) {
+		}
+		try {
+			
+			ImageView iconBack = (ImageView) findViewById(R.id.logo_img_reversed);
+			if ("es".equals(Locale.getDefault().getLanguage())) {
+				iconBack.setImageResource(R.drawable.icon_back_es);
+			}
+			RelativeLayout back=(RelativeLayout) findViewById(R.id.linear_back);
+			back.setOnClickListener(new View.OnClickListener() {
+
+				public void onClick(View v) {
+					Intent intent = null;
+					intent = new Intent(getApplicationContext(), LoginActivity.class);
+					startActivity(intent);
+					finish();
+					
+				}  
+			});
+			
+		} catch (Exception e) {
+		}
+		
 	}
 
 }
