27d78c755e789c176597c9f7befe1502edbbca8e
Orlando Aliaga <oaliaga@gmail.com>
2018-07-17 19:21:07 -0400

    Fix boot

--- app/src/main/java/com/prey/receivers/PreyBootController.java
+++ app/src/main/java/com/prey/receivers/PreyBootController.java
@@ -6,17 +6,13 @@
  ******************************************************************************/
 package com.prey.receivers;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 
 import com.prey.PreyConfig;
 import com.prey.PreyLogger;
 import com.prey.PreyPermission;
-import com.prey.R;
 import com.prey.actions.aware.AwareConfig;
 import com.prey.json.actions.Report;
 import com.prey.preferences.DisablePowerCheckBoxPreference;
@@ -33,25 +29,37 @@ public class PreyBootController extends BroadcastReceiver {
             if (interval != null && !"".equals(interval)) {
                 Report.run(context, Integer.parseInt(interval));
             }
-            final Context ctx=context;
+            final Context ctx = context;
             new Thread() {
                 public void run() {
                     AwareConfig.getAwareConfig(ctx).init();
                 }
             }.start();
-            boolean disablePowerOptions = PreyConfig.getPreyConfig(context).isDisablePowerOptions();
-            if (disablePowerOptions) {
-                context.startService(new Intent(context, PreyDisablePowerOptionsService.class));
-                DisablePowerCheckBoxPreference.notifyReady(context);
-            } else {
-                context.stopService(new Intent(context, PreyDisablePowerOptionsService.class));
-                DisablePowerCheckBoxPreference.notifyCancel(context);
-            }
-            if (PreyConfig.getPreyConfig(context).isLockSet()) {
-                if(PreyConfig.getPreyConfig(context).isMarshmallowOrAbove() && PreyPermission.canDrawOverlays(context)) {
-                    context.startService(new Intent(context, PreyLockService.class));
+            new Thread() {
+                public void run() {
+                    try {
+                        boolean disablePowerOptions = PreyConfig.getPreyConfig(ctx).isDisablePowerOptions();
+                        if (disablePowerOptions) {
+                            ctx.startService(new Intent(ctx, PreyDisablePowerOptionsService.class));
+                            DisablePowerCheckBoxPreference.notifyReady(ctx);
+                        } else {
+                            ctx.stopService(new Intent(ctx, PreyDisablePowerOptionsService.class));
+                            DisablePowerCheckBoxPreference.notifyCancel(ctx);
+                        }
+                    }catch (Exception e){}
                 }
-            }
+            }.start();
+            new Thread() {
+                public void run() {
+                    try {
+                        if (PreyConfig.getPreyConfig(ctx).isLockSet()) {
+                            if(PreyConfig.getPreyConfig(ctx).isMarshmallowOrAbove() && PreyPermission.canDrawOverlays(ctx)) {
+                                ctx.startService(new Intent(ctx, PreyLockService.class));
+                            }
+                        }
+                    }catch (Exception e){}
+                }
+            }.start();
         } else
             PreyLogger.e("Received unexpected intent " + intent.toString(), null);
     }
