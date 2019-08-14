20456acb028538fb4f4176bdc70b6ddd6a017350
Orlando Aliaga <oaliaga@gmail.com>
2019-08-14 17:36:49 -0400

    New logic for alert action

--- app/src/main/java/com/prey/activities/PopUpAlertActivity.java
+++ app/src/main/java/com/prey/activities/PopUpAlertActivity.java
@@ -8,23 +8,41 @@ package com.prey.activities;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
-import android.content.pm.ActivityInfo;
-import android.graphics.Color;
-import android.graphics.Typeface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.Window;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
+import com.prey.PreyConfig;
+import com.prey.PreyLogger;
 import com.prey.R;
 
 public class PopUpAlertActivity extends PreyActivity {
 
     private static final int SHOW_POPUP = 0;
     private String message = null;
+    private int notificationId = 0;
+
+    public static final String POPUP_PREY = "popup_prey";
+    private final BroadcastReceiver close_prey_receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            PreyLogger.d("PopUpAlertActivity close_prey_receiver finish");
+            finish();
+        }
+    };
+
+    private final BroadcastReceiver popup_prey_receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            PreyLogger.d("PopUpAlertActivity popup_prey_receiver finish");
+            PreyConfig.getPreyConfig(context).setNoficationPopupId(0);
+            finish();
+            }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,30 +50,42 @@ public class PopUpAlertActivity extends PreyActivity {
         Bundle bundle = this.getIntent().getExtras();
         if (bundle != null) {
             this.message = bundle.getString("alert_message");
+            this.notificationId = bundle.getInt("notificationId");
         }
-
-        showDialog(SHOW_POPUP);
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(PopUpAlertActivity.this);
+        alertBuilder.setTitle(R.string.popup_alert_title);
+        alertBuilder.setMessage(this.message);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setNeutralButton(R.string.close_alert, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                NotificationManager nMgr = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                nMgr.cancel(notificationId);
+                finish();
+            }
+        });
+        Dialog popup = alertBuilder.create();
+        popup.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            public void onDismiss(DialogInterface dialog) {
+                finish();
+            }
+        });
+        popup.show();
+        registerReceiver(close_prey_receiver, new IntentFilter(CheckPasswordHtmlActivity.CLOSE_PREY));
+        registerReceiver(popup_prey_receiver, new IntentFilter(POPUP_PREY + "_" + notificationId));
     }
 
     @Override
-    protected Dialog onCreateDialog(int id) {
-
-        Dialog popup = null;
-        switch (id) {
-
-            case SHOW_POPUP:
-                popup = new AlertDialog.Builder(PopUpAlertActivity.this).setTitle(R.string.popup_alert_title).setMessage(this.message)
-                        .setCancelable(true).create();
-
-                popup.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    public void onDismiss(DialogInterface dialog) {
-                        finish();
-                    }
-                });
+    protected void onResume() {
+        super.onResume();
+        int noficationPopupId = PreyConfig.getPreyConfig(this).getNoficationPopupId();
+        PreyLogger.d("PopUpAlertActivity onResume noficationPopupId:" + noficationPopupId);
+        if (noficationPopupId == 0) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY);
+            startActivity(intent);
         }
-        return popup;
     }
-
 
 }
 
