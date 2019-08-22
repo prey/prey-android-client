c1153d930e0b8a2a708a376faba52174334bc851
Orlando Aliaga <oaliaga@gmail.com>
2019-08-22 16:15:34 -0400

    New version 2.1.6

--- app/src/main/java/com/prey/PreyConfig.java
+++ app/src/main/java/com/prey/PreyConfig.java
@@ -44,7 +44,7 @@ public class PreyConfig {
 
     private static final String HTTP="https://";
 
-    public static final String VERSION_PREY_DEFAULT="2.1.5";
+    public static final String VERSION_PREY_DEFAULT="2.1.6";
 
     // Milliseconds per second
     private static final int MILLISECONDS_PER_SECOND = 1000;
