644ef34c4f896fcfd7a8915edc85f2de09fa74cc
Orlando Aliaga <oaliaga@gmail.com>
2019-09-30 13:58:18 -0300

    New command sync name

---
+++ app/src/main/java/com/prey/json/actions/SyncName.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Created by Carlos Yaconi
+ * Copyright 2019 Prey Inc. All rights reserved.
+ * License: GPLv3
+ * Full license at "/LICENSE"
+ ******************************************************************************/
+package com.prey.json.actions;
+
+import android.content.Context;
+import android.os.Build;
+import android.provider.Settings;
+
+import com.prey.PreyLogger;
+import com.prey.actions.observer.ActionResult;
+import com.prey.backwardcompatibility.AboveCupcakeSupport;
+import com.prey.events.Event;
+import com.prey.events.manager.EventManagerRunner;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class SyncName {
+
+    public void start(final Context ctx, List<ActionResult> list, JSONObject parameters) {
+        new Thread() {
+            public void run() {
+                try {
+                    String model = Build.MODEL;
+                    String vendor = "Google";
+                    try {
+                        vendor = AboveCupcakeSupport.getDeviceVendor();
+                    } catch (Exception e) {
+                    }
+                    String newName = vendor + " " + model;
+                    String name = Settings.Secure.getString(ctx.getContentResolver(), "bluetooth_name");
+                    if (name != null && !"".equals(name)) {
+                        newName = newName + ", " + name;
+                    }
+                    PreyLogger.d("new_name:" + newName);
+                    JSONObject info = new JSONObject();
+                    info.put("new_name", newName);
+                    Event event = new Event(Event.DEVICE_RENAMED, info.toString());
+                    new EventManagerRunner(ctx, event).run();
+                } catch (Exception e) {
+                }
+            }
+        }.start();
+    }
+}
