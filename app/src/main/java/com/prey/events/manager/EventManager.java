760345e95f0c5426c58a65a06c81abd21dd73e3f
Orlando Aliaga <oaliaga@gmail.com>
2019-12-19 13:06:58 -0300

    New device event added

--- app/src/main/java/com/prey/events/manager/EventManager.java
+++ app/src/main/java/com/prey/events/manager/EventManager.java
@@ -23,6 +23,7 @@ import com.prey.events.retrieves.EventRetrieveDataNullMobile;
 import com.prey.events.retrieves.EventRetrieveDataOnline;
 import com.prey.events.retrieves.EventRetrieveDataUptime;
 import com.prey.events.retrieves.EventRetrieveDataWifi;
+import com.prey.events.retrieves.EventRetrieveDataMinWifi;
 import com.prey.managers.PreyConnectivityManager;
 
 public class EventManager {
@@ -77,7 +78,7 @@ public class EventManager {
             } else {
                 validation = false;
             }
-            if(event.getInfo().indexOf("mobile")>0){
+            if(event.getInfo().indexOf(EventManager.MOBILE)>0){
                 boolean isMobileConnected=false;
                 for (int i = 0; i < 10; i++) {
                     try {
@@ -101,29 +102,52 @@ public class EventManager {
         if(event.isAlwaysSend()){
             validation=true;
         }
+        boolean isMobileConnected=PreyConnectivityManager.getInstance(ctx).isMobileConnected();;
+        for (int i = 0; isMobileConnected&&i < 10; i++) {
+            String networkClass= PreyPhone.getNetworkClass(ctx);
+            PreyConfig.getPreyConfig(ctx).setPreviousSsid(networkClass);
+            type_connect="mobile";
+            break;
+        }
+
         PreyLogger.d("EVENT name:" + event.getName() + " info:" + event.getInfo() + " ssid[" + ssid + "] previousSsid[" + previousSsid + "]");
         PreyLogger.d("validation:"+validation);
         if (validation) {
             PreyLogger.d("EVENT change PreviousSsid:" + ssid);
             PreyConfig.getPreyConfig(ctx).setPreviousSsid(ssid);
-            // if This Device Already Registered With Prey
             if (isDeviceRegistered) {
-                        PreyLogger.d("EVENT isDeviceRegistered");
+                        PreyLogger.d("EVENT isDeviceRegistered event: "+event.getName()+" type_connect:"+type_connect);
                         this.mapData = new EventMap<String, JSONObject>();
                         this.event = event;
-                        this.mapData.put(EventManager.ONLINE, null);
-                        this.mapData.put(EventManager.UPTIME, null);
-                        this.mapData.put(EventManager.WIFI, null);
+                        if (Event.DEVICE_STATUS.equals(event.getName())) {
+                            if(EventManager.MOBILE.equals(type_connect)) {
+                                this.mapData.put(EventManager.MOBILE, null);
+                            }else {
+                                this.mapData.put(EventManager.WIFI, null);
+                            }
+                        }else{
+                            this.mapData.put(EventManager.MOBILE, null);
+                            this.mapData.put(EventManager.WIFI, null);
+                            this.mapData.put(EventManager.ONLINE, null);
+                            this.mapData.put(EventManager.UPTIME, null);
+                        }
                         this.mapData.put(EventManager.BATTERY, null);
-                        this.mapData.put(EventManager.MOBILE, null);
-                        new EventRetrieveDataOnline().execute(ctx, this);
-                        new EventRetrieveDataUptime().execute(ctx, this);
-                        new EventRetrieveDataWifi().execute(ctx, this);
+                        if (Event.DEVICE_STATUS.equals(event.getName())) {
+                            if(EventManager.MOBILE.equals(type_connect)) {
+                                new EventRetrieveDataMobile().execute(ctx, this);
+                            }else {
+                                new EventRetrieveDataMinWifi().execute(ctx, this);
+                            }
+                        }else{
+                            if(EventManager.MOBILE.equals(type_connect))
+                                new EventRetrieveDataMobile().execute(ctx, this);
+                            else
+                                new EventRetrieveDataNullMobile().execute(ctx, this);
+                            new EventRetrieveDataWifi().execute(ctx, this);
+                            new EventRetrieveDataOnline().execute(ctx, this);
+                            new EventRetrieveDataUptime().execute(ctx, this);
+                        }
                         new EventRetrieveDataBattery().execute(ctx, this);
-                        if("mobile".equals(type_connect))
-                            new EventRetrieveDataMobile().execute(ctx, this);
-                        else
-                            new EventRetrieveDataNullMobile().execute(ctx, this);
             }
         }
     }
@@ -153,11 +177,15 @@ public class EventManager {
                             }
                         }
                     }
+                    if(Event.DEVICE_STATUS.equals(event.getName())){
+                        event.setInfo(jsonObjectStatus.toString());
+                    }
                     if (!Event.WIFI_CHANGED.equals(event.getName()) || !event.getName().equals(lastEvent)) {
                         PreyConfig.getPreyConfig(ctx).setLastEvent(event.getName());
                         PreyLogger.d("event name[" + this.event.getName() + "], info[" + this.event.getInfo() + "]");
                         new EventThread(ctx, event, jsonObjectStatus).start();
                     }
+
             }
         }
     }
