8849b818b704985bb0702a4c041e2253f6160861
Orlando Aliaga <oaliaga@gmail.com>
2024-10-11 10:29:10 -0300

    Documentation is added
    Documentation is added

--- app/src/main/java/com/prey/workers/PreyWorker.java
+++ app/src/main/java/com/prey/workers/PreyWorker.java
@@ -14,14 +14,25 @@ import androidx.work.WorkManager;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * This class represents a worker that starts a periodic worker to run every 15 minutes, with a 5 minute flex period.
+ */
 public class PreyWorker {
 
     private static PreyWorker _instance = null;
     public static final String INCREMENT_WORK_NAME = "prey_increment_work";
 
+    /**
+     * Private constructor to prevent instantiation of this class.
+     */
     private PreyWorker() {
     }
 
+    /**
+     * Returns the singleton instance of the PreyWorker.
+     *
+     * @return the singleton instance of PreyWorker
+     */
     public static PreyWorker getInstance() {
         if (_instance == null)
             _instance = new PreyWorker();
@@ -35,14 +46,17 @@ public class PreyWorker {
     public void startPeriodicWork(Context context) {
         WorkManager workManager = WorkManager.getInstance(context);
         workManager.cancelAllWorkByTag(INCREMENT_WORK_NAME);
-        PeriodicWorkRequest workRequest = new PeriodicWorkRequest.Builder(
+        // Create a PeriodicWorkRequest builder
+        PeriodicWorkRequest.Builder builder = new PeriodicWorkRequest.Builder(
                 IncrementWorker.class,
                 15,
                 TimeUnit.MINUTES,
                 5,
                 TimeUnit.MINUTES)
-                .addTag(INCREMENT_WORK_NAME)
-                .build();
+                .addTag(INCREMENT_WORK_NAME);
+        // Build the PeriodicWorkRequest
+        PeriodicWorkRequest workRequest = builder.build();
+        // Enqueue the work request
         workManager.enqueue(workRequest);
     }
 
