25738c7b75715373bb22055638a26bf8160cdd39
Orlando Aliaga <oaliaga@gmail.com>
2024-11-15 12:00:08 -0300

    Add enrollment-specific ID
    Add enrollment-specific ID

--- app/src/main/java/com/prey/receivers/PreyBootController.java
+++ app/src/main/java/com/prey/receivers/PreyBootController.java
@@ -9,7 +9,9 @@ package com.prey.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.RestrictionsManager;
 import android.os.Build;
+import android.os.Bundle;
 
 import com.prey.PreyConfig;
 import com.prey.PreyLogger;
@@ -83,6 +85,21 @@ public class PreyBootController extends BroadcastReceiver {
                         PreyBetaController.startPrey(ctx);
                     } catch (Exception e) {
                         PreyLogger.e("Error:"+e.getMessage(),e);
+                    }
+                }
+            }.start();
+            new Thread() {
+                public void run() {
+                    // Get the RestrictionsManager instance
+                    RestrictionsManager manager = (RestrictionsManager) context.getSystemService(Context.RESTRICTIONS_SERVICE);
+                    // Get the application restrictions
+                    Bundle applicationRestrictions = manager.getApplicationRestrictions();
+                    // Check if application restrictions are not null
+                    if (applicationRestrictions != null) {
+                        // Log the application restrictions
+                        PreyLogger.d(String.format("RestrictionsReceiver restrictions applied: %s", applicationRestrictions.toString()));
+                        // Handle the application restrictions
+                        RestrictionsReceiver.handleApplicationRestrictions(context, applicationRestrictions);
                     }
                 }
             }.start();
